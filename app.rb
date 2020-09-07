7c478e3b9170b8eaa86feedb77cb869efb21dabf
Christie Yoon <cleex1999@gmail.com>
2020-09-07 22:16:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,25 @@
-require_relative 'config/environment'
+require_relative '/config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
 
+  # get '/reversename/:name' do
+  #     @name = params[:name]
+  #     "#{@name}.reverse"
+  # end
 end
+
+  # get '/square/:number' do
+  #
+  # end
+  #
+  # get '/say/:number/:phrase' do
+  #
+  # end
+  #
+  # get get '/say/:word1/:word2/:word3/:word4/:word5' do
+  #
+  # end
+  #
+  # get get '/:operation/:number1/:number2' do
+  #
+  # end
