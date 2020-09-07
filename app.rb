8106207e585bd34f1fb92c6aa7e7905881324bda
Christie Yoon <cleex1999@gmail.com>
2020-09-07 22:26:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,13 +10,13 @@ class App < Sinatra::Base
 
   get '/square/:number' do
       @num = params[:number].to_i
-      "#{@num**2}"
+      "#{@num}**2"
   end
 
-  # get '/say/:number/:phrase' do
-  #
-  # end
-  #
+  get '/say/:number/:phrase' do
+      
+  end
+  
   # get get '/say/:word1/:word2/:word3/:word4/:word5' do
   #
   # end
