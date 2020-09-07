676bf8700ce3c64ffb80bf73d1ea0c46b972ab42
Christie Yoon <cleex1999@gmail.com>
2020-09-07 23:42:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ class App < Sinatra::Base
 
   get '/say/:number/:phrase' do
       @num = params[:number].to_i
-      @phrase = params[:phrase].to_s
+      @phrase = params[:phrase]
       "#{@num.times "#{@phrase}"}"
       binding.pry
   end
